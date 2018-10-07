38807c60b1007c8b067ba90af27f6b99700612c9
Adzwoolly <admwoollen@gmail.com>
2018-10-07 01:09:12 +0100

    Implement local search

--- src/main/java/uk/adamwoollen/travellingsalesperson/Main.java
+++ src/main/java/uk/adamwoollen/travellingsalesperson/Main.java
@@ -1,10 +1,12 @@
 package uk.adamwoollen.travellingsalesperson;
 
+import uk.adamwoollen.travellingsalesperson.solutions.LocalSearch;
 import uk.adamwoollen.travellingsalesperson.solutions.Random;
 
 import java.util.StringJoiner;
 
-public class Main {
+public class Main
+{
 	public static void main(String[] args)
 	{
 		System.out.println("Hello, world!");
@@ -14,7 +16,8 @@ public class Main {
 	public static double getRouteCost(double[][] graph, int[] route)
 	{
 		double cost = 0;
-		for (int i = 0; i < route.length - 1; i++) {
+		for (int i = 0; i < route.length - 1; i++)
+		{
 			cost += graph[route[i]][route[i + 1]];
 		}
 		return cost;
@@ -26,16 +29,30 @@ public class Main {
 
 		GraphBuilder gb = new GraphBuilder();
 		double[][] graph = gb.buildGraphFromCsv("ulysses16.csv");
+
+		LineChart lineChart = new LineChart();
 		
 		System.out.println("Calculating, please wait...");
 
-		int[] bestRouteFound = Random.solveUsingRandom(graph, new LineChart(), 10);
+		int[] bestRouteFound = Random.solveUsingRandom(graph, lineChart, 10);
+		double bestRouteFoundCost = getRouteCost(graph, bestRouteFound);
+		String bestRouteFoundSolutionName = "Random";
+
+		int[] localSearchRouteFound = LocalSearch.solveUsingLocalSearch(graph, lineChart, 10);
+		double localSearchRouteFoundCost = getRouteCost(graph, localSearchRouteFound);
+		if (localSearchRouteFoundCost < bestRouteFoundCost)
+		{
+			bestRouteFound = localSearchRouteFound;
+			bestRouteFoundCost = localSearchRouteFoundCost;
+			bestRouteFoundSolutionName = "Local Search";
+		}
 
 		System.out.println("===================");
 		System.out.println("CHEAPEST ROUTE");
 
+		System.out.println(bestRouteFoundSolutionName);
 		Main.printRoute(bestRouteFound);
-		System.out.println(getRouteCost(graph, bestRouteFound));
+		System.out.println(bestRouteFoundCost);
 
 		System.out.println("Total time to execute program: " + ((System.nanoTime() - startTime) / 1000000) + " milliseconds");
 	}
---
+++ src/main/java/uk/adamwoollen/travellingsalesperson/solutions/LocalSearch.java
@@ -0,0 +1,83 @@
+package uk.adamwoollen.travellingsalesperson.solutions;
+
+import uk.adamwoollen.travellingsalesperson.Main;
+import uk.adamwoollen.travellingsalesperson.SolutionLog;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class LocalSearch
+{
+    public static int[] solveUsingLocalSearch(double[][] graph, SolutionLog solutionLog, int secondsToRunFor)
+    {
+        long startTime = System.nanoTime();
+        long endTime = startTime + (secondsToRunFor * 1000000000L);
+
+        int[] cheapestRoute = null;
+        double cheapestRouteCost = Double.MAX_VALUE;
+        while(System.nanoTime() < endTime)
+        {
+            int[] cheapestNeighbourhoodRoute = Random.getRandomRoute(graph.length);
+            int[] neighbouringRoute = cheapestNeighbourhoodRoute;
+            // Keep searching the neighbourhood until the cheapest route in the neighbourhood is found
+            while(!Arrays.equals(neighbouringRoute, cheapestNeighbourhoodRoute))
+            {
+                neighbouringRoute = getShortestRouteInTwoOptNetwork(graph, cheapestNeighbourhoodRoute);
+            }
+
+            double cheapestNeighbourhoodRouteCost = Main.getRouteCost(graph, cheapestNeighbourhoodRoute);
+
+            if (cheapestNeighbourhoodRouteCost < cheapestRouteCost)
+            {
+                cheapestRoute = cheapestNeighbourhoodRoute;
+                cheapestRouteCost = cheapestNeighbourhoodRouteCost;
+
+                long timeElapsed = ((System.nanoTime() - startTime) / 1000000);
+                solutionLog.logSolution("local search", timeElapsed, cheapestNeighbourhoodRouteCost);
+            }
+        }
+        return cheapestRoute;
+    }
+
+    private static int[] getShortestRouteInTwoOptNetwork(double[][] graph, int[] route)
+    {
+        List<int[]> twoOptNetwork = getTwoOptNetwork(route);
+
+        int[] cheapestRoute = null;
+        double cheapestRouteCost = Double.MAX_VALUE;
+
+        for (int[] neighbouringRoute : twoOptNetwork)
+        {
+            double routeCost = Main.getRouteCost(graph, neighbouringRoute);
+            if(routeCost < cheapestRouteCost)
+            {
+                cheapestRouteCost = routeCost;
+                cheapestRoute = route;
+            }
+        }
+        return cheapestRoute;
+    }
+
+    private static List<int[]> getTwoOptNetwork(int[] route)
+    {
+        List<int[]> twoOptNetwork = new ArrayList<>();
+        for (int i = 0; i < route.length; i++)
+        {
+            for (int j = 0; j < route.length; j++)
+            {
+                twoOptNetwork.add(switchNodes(route, i, j));
+            }
+        }
+        return twoOptNetwork;
+    }
+
+    private static int[] switchNodes(int[] route, int firstNode, int secondNode)
+    {
+        int[] newRoute = route.clone();
+        int tempNode = newRoute[firstNode];
+        newRoute[firstNode] = newRoute[secondNode];
+        newRoute[secondNode] = tempNode;
+        return newRoute;
+    }
+}
